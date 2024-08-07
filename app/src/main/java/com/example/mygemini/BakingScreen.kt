c88bde80bb6b202c6c6772b86022df133ee2eca1
aman <amanavelon@gmail.com>
2024-08-07 17:00:13 +0530

    rounded corner fixed

--- app/src/main/java/com/example/mygemini/BakingScreen.kt
+++ app/src/main/java/com/example/mygemini/BakingScreen.kt
@@ -33,9 +33,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -56,7 +58,8 @@ fun ChatScreen(
             elevation = CardDefaults.cardElevation(
                 defaultElevation = 6.dp
             ),
-            modifier = Modifier.fillMaxWidth().height(60.dp)
+            modifier = Modifier.fillMaxWidth().height(60.dp),
+            shape = RectangleShape
         ) {
             Box(
                 modifier = Modifier
@@ -72,7 +75,6 @@ fun ChatScreen(
             }
         }
 
-        // Display chat messages
         LazyColumn(
             modifier = Modifier
                 .weight(1f)
@@ -94,7 +96,6 @@ fun ChatScreen(
             }
         }
 
-        // User input field and send button
         Row(
             modifier = Modifier.padding( 8.dp)
         ) {
@@ -152,4 +153,20 @@ fun ChatMessage(message: ChatMessage) {
                 .padding(8.dp)
         )
     }
+}
+
+@Preview
+@Composable
+fun ChatScreenPreview() {
+    ChatScreen(chatViewModel = ChatViewModel())
+}
+@Preview(showBackground = true)
+@Composable
+fun ChatMessagePreview() {
+    ChatMessage(
+        message = ChatMessage(
+            text = "This is a preview message",
+            sender = MessageSender.USER
+        )
+    )
 }
